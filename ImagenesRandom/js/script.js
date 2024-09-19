12455fb81b9036a5fcdb3b5d940adcf751207338
Lubna-20 <150439871+Lubna-20@users.noreply.github.com>
2024-09-19 05:45:04 -0300

    m

--- ImagenesRandom/js/script.js
+++ ImagenesRandom/js/script.js
@@ -10,13 +10,7 @@ function inicio(){
                 const imagenX=document.createElement("img");
                 //Añadir al atributo src de la etiqueta img un valor
                 let numeroRandom=Math.round(Math.random()*20)
-                if(indice % 2 == 0){
                     imagenX.setAttribute("src","https://randomuser.me/api/portraits/women/" + numeroRandom+".jpg")
-
-                } else {
-                    imagenX.setAttribute("src","https://randomuser.me/api/portraits/men/"+ numeroRandom+".jpg")
-                }
-                divSecundario.appendChild(imagenX)
 
             })
 
