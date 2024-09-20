0894aafd5d9e4307296d8f665fb49402f41ed9ed
Lubna-20 <150439871+Lubna-20@users.noreply.github.com>
2024-09-20 08:40:22 -0300

    coloresRandom

---
+++ JuegoColores/js/script.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', inicio)
+
+function inicio(){
+    vectoresColores = ['red','green','blue','pink']
+    vectoresNombre = ['#ff0000','#00bb2d','#0000ff','#ff0080']
+    
+}
