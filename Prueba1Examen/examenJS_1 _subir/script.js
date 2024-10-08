5036385dfaa43627901450cdbe1fa4797715a5d4
Lubna-20 <lmeziane02@educarex.es>
2024-10-08 11:12:24 +0200

    ff

--- Prueba1Examen/examenJS_1 _subir/script.js
+++ Prueba1Examen/examenJS_1 _subir/script.js
@@ -1,69 +1,59 @@
-window.addEventListener('load', inicio)
-let btn = document.getElementById('validar')
-let validar = true
+let accion = document.querySelector('form')
+let validar = true;
 
-function inicio(){
-    let formulario = document.querySelector('form')
+accion.onsubmit = ()=>{
+    //Declaramos los controladores, validaciones y small
+    let validacion1 = document.getElementById('v1').value
+    let validacion2 = document.getElementById('v2').value
+    let validacion3 = document.getElementById('v3').value
+    let validacion4 = document.getElementById('v4').value
+ 
+    let smvalidacion1 = document.getElementById('sm1')
+    let smvalidacion2 = document.getElementById('sm2')
+    let smvalidacion3 = document.getElementById('sm3')
+    let smvalidacion4 = document.getElementById('sm4')
 
-    btn.click = ()=>{
-        let valid1 = document.querySelector('#v1').value
-        let smv1 = document.querySelector('#smv1');
-    
-        let valid2 = document.querySelector('#v2').value
-        let smv2 = document.querySelector('#smv2');
-    
-        let valid3 = document.querySelector('#v3').value
-        let smv3 = document.querySelector('#smv4');
-    
-        let valid4 = document.querySelector('#v4').value
-        let smv4 = document.querySelector('#smv4');
-    
-        // Validación 
-        validacion1(valid1, smv1);
-     
-        validacion2(valid2, smv2);
-    
-        validacion3(valid3, smv3);
-      
-        validacion4(valid4, smv4);
-    
-         //funcion para valir
-    
-       
-        function validacion1(valid1, smv1){
-            if (valid1.length == 0) {
-                smv1.textContent="*CAMPO OBLIGATORIO"
-            }else if(valid1.length == 10){
-                smv1.textContent = "*El texto debe tener 10 caracteres";
-                validar = false;
-                console.log(variable)
-            }
+    //Creamos las funciones de cada validacion
+    validacion1campo(validacion1,smvalidacion1)
+    validacion2campo(validacion2,smvalidacion2)
+    validacion3campo(validacion3,smvalidacion3)
+    validacion4campo(validacion4,smvalidacion4)
 
-        }
-
-
-        function validacion4(variable, texto) {
-            let patronMayus = /[A-Z]/;// el patron buscado en internet
-            
-    
-            if (variable.length == 0) {
-                texto.textContent = "*CAMPO OBLIGATORIO";
-                validar = false;
-            } else if (variable.length !== 8) {
-                texto.textContent = "*El texto debe tener 8 caracteres";
-                validar = false;
-            } else if (!patronMayus.test(variable) || !patronMayus.test(variable)) {
-                texto.textContent = "*El texto debe tener al menos una letra MAYÚSCULA y un número";
-                validar = false;
-            } else {
-                texto.textContent = ""; 
-            }
-            console.log(variable)
-        }
-    
-        return validar;
-        
-    }
+    return validar
 }
 
-
+function validacion1campo(v1,texto){
+    //Creamos la condicion
+    if (v1.length != 10) {
+        validar=false
+        texto.textContent="El campo debe tener exactamente 10 caracteres"
+    }else if(isNaN(v1[0]) || isNaN(v1[v1.length-1])){
+        texto.textContent="Cumples la longitud de 10,peor el primero y el ulimo deben ser un numero"
+    }
+}
+function validacion2campo(v2,texto){
+    if (v2.length != 12) {
+        validar=false
+        texto.textContent="El campo debe tener exactamente 12 caracteres"
+    }else {
+        for (let i = 0; i< v2.length; i++) {
+          if (!isNaN(v2.chartAt(i)) && v2.length/2) {
+            validar=false
+            texto.textContent="Cumples la longitud de 12, en los primeros 6 hay al menos un numero"
+          }
+           
+        }
+    }
+}
+function validacion3campo(v3,texto){
+    if (v3.length != 20) {
+        validar=false
+        texto.textContent="El campo debe tener exactamente 20 caracteres"
+    }
+}
+function validacion4campo(v4,texto){
+    if (v4.length != 8) {
+        validar=false
+        texto.textContent="El campo debe tener exactamente 8 caracteres"
+    }
+}
