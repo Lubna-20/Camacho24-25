06206fa4f4167fa4e382809e81422b1fd88ec160
Lubna-20 <lmeziane02@educarex.es>
2025-01-24 12:54:59 +0100

    casa

---
+++ AngularCargaPerezoasa/superProyecto/src/app/proyecto2/proyecto2.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Proyecto2RoutingModule } from './proyecto2-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    Proyecto2RoutingModule
+  ]
+})
+export class Proyecto2Module { }
