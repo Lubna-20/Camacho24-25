641b16c8145d3732c5c2242139dee43d86ced116
Lubna-20 <lmeziane02@educarex.es>
2025-01-31 12:45:00 +0100

    agregar campo para destinatario en el chat y mejorar la lógica de envío de mensajes

--- AngularCargaPerezoasa/chatdaw/src/app/chat-p/chat-p.component.ts
+++ AngularCargaPerezoasa/chatdaw/src/app/chat-p/chat-p.component.ts
@@ -1,81 +1,96 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Mensaje } from '../mensaje';
 import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { ServicioClienteService } from '../servicio-cliente.service';
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
+import { Usuario } from '../usuario';
 
 @Component({
   selector: 'app-chat-p',
   templateUrl: './chat-p.component.html',
   styleUrls: ['./chat-p.component.css']
 })
-export class ChatPComponent {
+export class ChatPComponent  implements OnInit {
+  uSuario: any;
+  miParametro!:string;
 
+constructor( private httpClient:ServicioClienteService){
 
-
-    uSuario!:string|null
-    mensaje:Mensaje={
-      id:0,
-      fecha:'',
-      usuario:'',
-      mensaje:'',
-      destinatario:'rodos',
-      activo:1
-
-    }
-    dataSource = new MatTableDataSource<Mensaje>()
-
- constructor(private router:Router,private servicioCliente:ServicioClienteService){
-
-  }
-  recargar() {
-    this.servicioCliente.leerMensajes().subscribe((resultado:Mensaje[])=>{
-      this.dataSource.data=resultado
-      this.dataSource.paginator=this.paginator
-      this.dataSource.sort=this.sort
-
-    })
-  }
-  enviarMensaje() {
-    this.mensaje.usuario = this.uSuario|| ''
-    this.mensaje.fecha= new Date().toLocaleDateString()
-  this.servicioCliente.escribirMensajes(this.mensaje).subscribe(()=>{
-    this.servicioCliente.leerMensajes().subscribe((resultado:Mensaje[])=>{
-      this.dataSource.data=resultado
-      this.dataSource.paginator=this.paginator
-      this.dataSource.sort=this.sort
-      this.tabla.renderRows();
-      this.dataSource._renderChangesSubscription;
-
-    })
-  })
-  }
-    cerrarSesion() {
-      sessionStorage.removeItem('Nombre')
-      this.uSuario = 'Sesión Cerrada'
-      this.dataSource = new MatTableDataSource<Mensaje>()
-      this.router.navigate(['login'])
-    }
-    displayedColumns: string []= ['id' ,'fecha', 'usuario','mensaje', 'destinatario']
-ngAfterViewInit() {
-  this.dataSource.paginator = this.paginator;
-  this.dataSource.sort = this.sort;
 }
-
-    applyFilter(event: Event) {
-      const filterValue = (event.target as HTMLInputElement).value;
-      this.dataSource.filter = filterValue.trim().toLowerCase();
-
-      if (this.dataSource.paginator) {
-        this.dataSource.paginator.firstPage();
-      }
+  ngOnInit(): void {
+    if (sessionStorage.getItem('Nombre')==null) {
+      this.dataSource=new MatTableDataSource<Mensaje>()
+    } else{
+      this.uSuario=sessionStorage.getItem('Nombre')||'';
+      this.httpClient.leerMensajeP(this.uSuario).subscribe(
+        (resultado:Mensaje[])=>{
+        this.dataSource.data=resultado
+        this.dataSource.paginator=this.paginator
+        this.dataSource.sort=this.sort
+       })
     }
-    @ViewChild(MatPaginator)
-      paginator!: MatPaginator;
-      @ViewChild(MatSort)
-      sort!: MatSort;
-    tabla!:MatTable<Mensaje>
+
+  }
+  insertarMensaje() {
+    this.msjchat.usuario=this.uSuario;
+    this.httpClient.escribirMensajes(this.msjchat).subscribe(()=>{
+      this.httpClient.leerMensajeP(this.uSuario).subscribe((datos: Mensaje[])=>{
+        this.dataSource.data=datos;
+        this.dataSource.paginator=this.paginator;
+        this.dataSource.sort=this.sort;
+        this.msjchat.mensaje='';
+      })
+    })
+  }
+  mandarPrivado(destinatario: string) {
+    this.msjchat.destinatario=destinatario;
+    this.httpClient.insertarMensajeP(this.msjchat).subscribe(()=>{
+      this.httpClient.leerMensajeP(this.uSuario).subscribe((datos: Mensaje[])=>{
+        this.dataSource.data=datos;
+        this.dataSource.paginator=this.paginator;
+        this.dataSource.sort=this.sort;
+        this.msjchat.mensaje='';
+      })
+    })
+  }
+  displayedColumns: string []= ['id' ,'fecha', 'usuario','mensaje','destinatario']
+//Construir la lista de datos que va a recoger los contenidos del servicio obtenerMensajes()
+dataSource= new MatTableDataSource<Mensaje>();
+applyFilter($event: KeyboardEvent) {
+
+}
+msjchat: Mensaje={
+  id: 0,
+  usuario: '',
+  fecha: '',
+  mensaje: '',
+  destinatario: '',
+  activo: 1,
+}
+recargar() {
+
+}
+enviarMensaje() {
+  this.msjchat.usuario==sessionStorage.getItem('Nombre')||'';
+  this.msjchat.fecha=new Date().toLocaleDateString()  
+this.httpClient.insertarMensajeP(this.msjchat).subscribe(()=>{
+
+})
+
+}
+cerrarSesion() {
+
+
+}
+@ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  @ViewChild(MatSort)
+  sort!: MatSort;
+
+
+
+
 
 }
--- AngularCargaPerezoasa/chatdaw/src/app/servicio-cliente.service.ts
+++ AngularCargaPerezoasa/chatdaw/src/app/servicio-cliente.service.ts
@@ -10,35 +10,30 @@ import { formatDate } from '@angular/common';
 })
 export class ServicioClienteService {
 
-  constructor(private http:HttpClient, @Inject(LOCALE_ID) public locales: string) { }
+  constructor(private http:HttpClient, @Inject(LOCALE_ID) public local: string) { }
 
-// http://moralo.atwebpages.com/menuAjax/chat/AltaMensaje.php
-
-//http://moralo.atwebpages.com/menuAjax/chat/ObtenerMensajes.php
 
 insertarUsuario(usuario:Usuario):Observable<Usuario>{
   return this.http.post<Usuario>('http://moralo.atwebpages.com/menuAjax/chat/AltaUsuario.php',usuario)
 }
 leerMensajes():Observable<Mensaje[]>{
-  return this.http.get<Mensaje[]>('http://moralo.atwebpages.com/menuAjax/chat/ObtenerMensajes.php')
+  return this.http.get<Mensaje[]>('http://moralo.atwebpages.com/menuAjax/chat/ObtenerMensajes2.php ')
 }
 escribirMensajes(mensaje:Mensaje):Observable<Mensaje>{
   return this.http.post<Mensaje>('http://moralo.atwebpages.com/menuAjax/chat/AltaMensaje.php',mensaje)
 }
 insertarMensajeP(msjchat: Mensaje):Observable<Mensaje>{
   let fecha = new Date();
-  msjchat.fecha=formatDate(fecha,'HH:mm:ss/dd/MM/yyyy',this.locales);
+  msjchat.fecha=formatDate(fecha,'HH:mm:ss/dd-MM-yyyy',this.local);
   return this.http.post<Mensaje>('http://camacho.atwebpages.com/chatDaw/AltaMensajeP.php', msjchat);
 }
 leerMensajeP(miParametro: string):Observable<Mensaje[]>{
-  return this.http.get<Mensaje[]>('http://camacho.atwebpages.com/chatDaw/ObtenerMensajesP.php'+miParametro);
+  return this.http.get<Mensaje[]>('http://camacho.atwebpages.com/chatDaw/ObtenerMensajesP.php?usuario='+miParametro);
+
 }
 
 logeo(usuario:Usuario):Observable<Usuario[]>{
   return this.http.get<Usuario[]>('http://moralo.atwebpages.com/menuAjax/chat/SeleccionarUsuario.php?email='+usuario.email+'&pwd='+usuario.pwd)
 }
-
-
-//http://moralo.atwebpages.com/menuAjax/chat/SeleccionarUsuario.php?email='+user.email+'&pwd='+user.pwd
 
 }
